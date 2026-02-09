2125b59cce355baaac265ccfc1d3ebe8732f86e1
Mohammad Al-yassin <mohammadalyassin2005@gmail.com>
2026-02-10 00:47:32 +0300

    add class Patient to file patients.py

--- patients.py
+++ patients.py
@@ -1,1 +1,14 @@
+class Patient:
+    count_patient_id=0
+    def __init__(self,first_name,last_name,email,age,accommodation):
+        Patient.count_student_id+=1
+        self.patient_id= Patient.count_student_id
+        self.first_name=first_name
+        self.last_name=last_name
+        self.email=email
+        self.age=age
+        self.accommodation=accommodation
+    def __str__(self):
+        return f"Patient : {self.count_student_id},{self.first_name},{self.last_name},{self.email},{self.age},{self.accommodation} "
+    
 
