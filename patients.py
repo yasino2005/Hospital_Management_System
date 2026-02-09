cac96e4837094abd9fabd3d97971c8561bbbea6b
Mohammad Al-yassin <mohammadalyassin2005@gmail.com>
2026-02-10 01:32:12 +0300

    Added some modifications: Removed the ID from the patient class because it is automatically generated in the database

--- patients.py
+++ patients.py
@@ -1,16 +1,13 @@
 import sqlite3
 class Patient:
-    count_patient_id=0
     def __init__(self,first_name,last_name,email,age,accommodation):
-        Patient.count_student_id+=1
-        self.patient_id= Patient.count_student_id
         self.first_name=first_name
         self.last_name=last_name
         self.email=email
         self.age=age
         self.accommodation=accommodation
     def __str__(self):
-        return f"Patient : {self.count_student_id},{self.first_name},{self.last_name},{self.email},{self.age},{self.accommodation} "
+        return f"Patient : {self.first_name},{self.last_name},{self.email},{self.age},{self.accommodation} "
     
 
 class Connect_To_db:
@@ -68,7 +65,7 @@ class servicesPatient:
         con=Connect_To_db.connect()
         c=con.cursor()
         c.execute("SELECT * FROM Patients WHERE patient_id = ?",(id,))
-        lst_std=c.fetchall()
+        lst_std=c.fetchone()
         con.close()
         return lst_std
     
