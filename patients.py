239c63c1bde7afdaddaad1524752281f43b982f0
Mohammad Al-yassin <mohammadalyassin2005@gmail.com>
2026-02-10 01:01:27 +0300

    add class Connect_To_db and add class servicesPatient and  create table Patient

--- patients.py
+++ patients.py
@@ -1,3 +1,4 @@
+import sqlite3
 class Patient:
     count_patient_id=0
     def __init__(self,first_name,last_name,email,age,accommodation):
@@ -12,3 +13,30 @@ class Patient:
         return f"Patient : {self.count_student_id},{self.first_name},{self.last_name},{self.email},{self.age},{self.accommodation} "
     
 
+class Connect_To_db:
+    ## connect to database
+    @staticmethod
+    def connect():
+       con = sqlite3.connect('database.db')
+    #    conect Foreign Keys
+       con.execute("PRAGMA foreign_keys = ON")
+       return con
+   
+class servicesPatient:
+    # create table Patients
+    @staticmethod
+    def create_tables_Patients():
+        con = Connect_To_db.connect()
+        cursor = con.cursor()
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Patients (
+            patient_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            first_name TEXT,
+            last_name TEXT,
+            email TEXT,
+            age INTEGER,
+            accommodation TEXT
+        )
+        ''')
+        con.commit()
+        con.close()
